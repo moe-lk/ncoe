ec1918dd783472f36684b2231dc3f55ca70aca7b
inzamshahib <91459399+inzamshahib@users.noreply.github.com>
2021-10-01 17:18:37 +0530

    Update Olresults.php

--- application/controllers/Olresults.php
+++ application/controllers/Olresults.php
@@ -9,15 +9,15 @@ class Olresults extends CI_Controller{
     }
 
 
-    public function checkolres($olind1 = NULL, $olind2 = NULL, $olind3 = NULL,$yea1=NULL,$yes2=NULL,$yea3=NULL){
+    public function checkolres($olind1 = NULL, $olind2 = NULL, $olind3 = NULL,$yea1=NULL,$yea2=NULL,$yea3=NULL){
         $data['title'] = 'O/L Results sheet';
-
+        $this->form_validation->set_rules('year1','Ol Year','required|Integer');
         $this->form_validation->set_rules('index1', 'Index Number', 'required|is_exist[stuents.olindex1]|is_exist[stuents.olindex2]|is_exist[stuents.olindex3]');
+        $this->form_validation->set_rules('year2','Ol Year','Integer');
         $this->form_validation->set_rules('index2', 'Index Number', 'is_exist[stuents.olindex1]|is_exist[stuents.olindex2]|is_exist[stuents.olindex3]');
+        $this->form_validation->set_rules('year3','Ol Year','Integer');
         $this->form_validation->set_rules('index3', 'Index Number', 'is_exist[stuents.olindex1]|is_exist[stuents.olindex2]|is_exist[stuents.olindex3]');
-        $this->form_validation->set_rules('year1', 'Ol Year', 'required|is_exist[stuents.year1]|is_exist[stuents.year2]|is_exist[stuents.year3]');
-        $this->form_validation->set_rules('year2', 'Ol Year', 'is_exist[stuents.year1]|is_exist[stuents.year2]|is_exist[stuents.year3]');
-        $this->form_validation->set_rules('year3', 'Ol Year', 'is_exist[stuents.year1]|is_exist[stuents.year2]|is_exist[stuents.year3]');
+        
         
         // $this->form_validation->set_rules('year1', );
 
@@ -29,26 +29,28 @@ class Olresults extends CI_Controller{
             $this->load->view('pages/ol', $data);
             $this->load->view('templates/footer');
         } else {
-            $olindex1 = $this->input->post('index1');
-            $olindex2 = $this->input->post('index2');
-            $olindex3 = $this->input->post('index3');
             $year1 = $this->input->post('year1');
+            $olindex1 = $this->input->post('index1');
             $year2 = $this->input->post('year2');
+            $olindex2 = $this->input->post('index2');
             $year3 = $this->input->post('year3');
+            $olindex3 = $this->input->post('index3');
             
+           
+           
 
             $attempts = $this->input->post('sel1');
             
             if($olindex1 != "" && $year1 !=""){
-                $data['olresults1'] = $this->olresults_model-> get_olresults1($olind1);
+                $data['olresults1'] = $this->olresults_model-> get_olresults1($olind1,$yea1);
             }
             else{
                 $data['olresults1'] = '';
                 $olindex1 = 'N/A';
-                $yea1= 'N/A';
+                $year1= 'N/A';
             }
             if(($olindex2 != "" && $olindex2 != $olindex1) && ($year2 != "" && $year2 != $year1)){
-                $data['olresults2'] = $this->olresults_model-> get_olresults2($olind2);
+                $data['olresults2'] = $this->olresults_model-> get_olresults2($olind2,$yea2);
                 // var_dump($data['olresults2']);
                 if(!empty($data['olresults2'])){
                     if($data['olresults2'][0]['year'] != $data['olresults1'][0]['year'] ){
@@ -67,9 +69,9 @@ class Olresults extends CI_Controller{
 
             }
             if(($olindex2 != "" && $olindex3 != "" && $olindex3 != $olindex1 && $olindex3 != $olindex2) && ($year2 != "" && $year3 != "" && $year3 != $year1 && $year3 != $year2)){
-                $data['olresults3'] = $this->olresults_model-> get_olresults3($olind3);
+                $data['olresults3'] = $this->olresults_model-> get_olresults3($olind3,$yea3);
                 if(!empty($data['olresults3']) && !empty($data['olresults2'])){
-                    if($data['olresults3'][0]['year'] != $data['olresults1'][0]['year'] && $data['olresults3'][0]['year'] != $data['olresults2'][0]['year']){
+                    if($data['olresults3'][0]['year'] != $data['olresults1'][0]['year'] && $data['olresults'][0]['year'] != $data['olresults2'][0]['year']){
                     
                     }else{
                         $data['olresults3'] = '';
@@ -83,8 +85,15 @@ class Olresults extends CI_Controller{
                 $year3 = 'N/A';
             }
             // var_dump($olindex2);
-            if ($olindex1 == "N/A" || $year1=="N/A") {
-                echo "<script>alert('Please enter a valid Index or Valid Year')</script>";
+            if ($olindex1 != "N/A" && empty($data['olresults1'])){
+                echo "<script>alert('Please enter a valid Index ')</script>";
+                $data['title'] = 'Enter O/L index number/numbers';
+                $this->load->view('templates/header');
+                $this->load->view('pages/ol',$data);
+                $this->load->view('templates/footer');
+            }
+            else if ($year1 != "N/A" && empty($data['olresults1'] )) {
+                echo "<script>alert('Please enter a valid year ')</script>";
                 $data['title'] = 'Enter O/L index number/numbers';
                 $this->load->view('templates/header');
                 $this->load->view('pages/ol',$data);
@@ -123,14 +132,14 @@ class Olresults extends CI_Controller{
                 $this->load->view('pages/ol',$data);
                 $this->load->view('templates/footer');
             }
-            elseif ($olindex1 != "N/A" && empty($data['olresults3'])) {
+            elseif ($olindex3 != "N/A" && empty($data['olresults3'])) {
                 echo "<script>alert('Please enter a valid Index3')</script>";
                 $data['title'] = 'Enter O/L index number/numbers';
                 $this->load->view('templates/header');
                 $this->load->view('pages/ol',$data);
                 $this->load->view('templates/footer');
             }
-            elseif ($year1 != "N/A" && empty($data['olresults3'])) {
+            elseif ($year3 != "N/A" && empty($data['olresults3'])) {
                 echo "<script>alert('Please enter a valid year3')</script>";
                 $data['title'] = 'Enter O/L index number/numbers';
                 $this->load->view('templates/header');
@@ -144,13 +153,14 @@ class Olresults extends CI_Controller{
                 $this->load->view('templates/footer');
             }
 
-            $this->session->set_userdata('olindex1', $olindex1);
-            $this->session->set_userdata('olindex2', $olindex2);
-            $this->session->set_userdata('olindex3', $olindex3);
+
+
             $this->session->set_userdata('year1', $year1);
+            $this->session->set_userdata('olindex1', $olindex1);
             $this->session->set_userdata('year2', $year2);
+            $this->session->set_userdata('olindex2', $olindex2);
             $this->session->set_userdata('year3', $year3);
-            
+            $this->session->set_userdata('olindex3', $olindex3);
             $this->session->set_userdata('attempts', $attempts);
         }
         // var_dump($data);
@@ -158,12 +168,14 @@ class Olresults extends CI_Controller{
     }
 
     public function checkpirolres(){
+
+        $this->form_validation->set_rules('year1','Ol Year','required|Integer');
         $this->form_validation->set_rules('index1', 'Index Number', 'required|is_exist[stuents.olindex1]|is_exist[stuents.olindex2]|is_exist[stuents.olindex3]');
+        $this->form_validation->set_rules('year2','Ol Year','Integer');
         $this->form_validation->set_rules('index2', 'Index Number', 'is_exist[stuents.olindex1]|is_exist[stuents.olindex2]|is_exist[stuents.olindex3]');
+        $this->form_validation->set_rules('year3','Ol Year','Integer');
         $this->form_validation->set_rules('index3', 'Index Number', 'is_exist[stuents.olindex1]|is_exist[stuents.olindex2]|is_exist[stuents.olindex3]');
-        $this->form_validation->set_rules('year1', 'Ol Year', 'required|is_exist[stuents.year1]|is_exist[stuents.year2]|is_exist[stuents.year3]');
-        $this->form_validation->set_rules('year2', 'Ol Year', 'is_exist[stuents.year1]|is_exist[stuents.year2]|is_exist[stuents.year3]');
-        $this->form_validation->set_rules('year3', 'Ol Year', 'is_exist[stuents.year1]|is_exist[stuents.year2]|is_exist[stuents.year3]');
+        
         
         if($this->form_validation->run() === FALSE){
             $data['title'] = 'Enter Mulika Pirivena index numbers';
@@ -171,24 +183,24 @@ class Olresults extends CI_Controller{
             $this->load->view('pages/olpirivena', $data);
             $this->load->view('templates/footer');
         } else {
-            $olindex1 = $this->input->post('index1');
-            $olindex2 = $this->input->post('index2');
-            $olindex3 = $this->input->post('index3');
             $year1 = $this->input->post('year1');
+            $olindex1 = $this->input->post('index1');
             $year2 = $this->input->post('year2');
+            $olindex2 = $this->input->post('index2');
             $year3 = $this->input->post('year3');
+            $olindex3 = $this->input->post('index3');
             $attempts = $this->input->post('sel1');
 
-            $this->session->set_userdata('olindex1', $olindex1);
-            $this->session->set_userdata('olindex2', $olindex2);
-            $this->session->set_userdata('olindex3', $olindex3);
             $this->session->set_userdata('year1', $year1);
+            $this->session->set_userdata('olindex1', $olindex1);
             $this->session->set_userdata('year2', $year2);
+            $this->session->set_userdata('olindex2', $olindex2);
             $this->session->set_userdata('year3', $year3);
+            $this->session->set_userdata('olindex3', $olindex3);
             $this->session->set_userdata('attempts', $attempts);
+          
 
             redirect('students/sripada');
         }
     }
 }
-
